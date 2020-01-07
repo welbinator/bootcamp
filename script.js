6516ca1c3b3aa082ed8b04d7c1a70e1d5bfa985d
jameswelbes <jameswelbes@penguin>
2020-01-07 16:22:48 -0600

    syllabus

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// sticky header animation 
+
 const syllabus = document.querySelector('#syllabus');
 const header = document.querySelector('header');
 const options = {
@@ -21,3 +23,25 @@ entries.forEach(entry => {
 
 
 observer.observe(syllabus);
+
+
+// accordion
+
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
